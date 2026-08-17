03c6d5a8946ae958117f06eb194ad0438ebc88be
Python tutorial <adeelahmed2005.goole.com.pk@gmail.com>
2026-08-18 01:17:58 +0500

    linear-regression

--- 24-ML_linear_regression.py
+++ 24-ML_linear_regression.py
@@ -1,13 +1,13 @@
 #Theory about this code
 
-This code demonstrates how to create a scatter plot using Matplotlib. First, the matplotlib.
-pyplot library is imported as plt, which provides functions for creating graphs and visualizing data.
-The x list contains numerical values that represent the x-axis, while the y list contains corresponding
-numerical values that represent the y-axis. The plt.scatter(x, y) function creates a scatter plot by placing
-each pair of values (x, y) as an individual point on the graph. Each point represents the relationship between
-the corresponding values from the two lists. Scatter plots are useful for identifying patterns, relationships,
-trends, and variations between two numerical variables. Finally, plt.show() displays the scatter plot on the screen. 
-Overall, this code provides a simple example of using a scatter plot to visualize the relationship between two sets of numerical data.
+#This code demonstrates how to create a scatter plot using Matplotlib. First, the matplotlib.
+#pyplot library is imported as plt, which provides functions for creating graphs and visualizing data.
+#The x list contains numerical values that represent the x-axis, while the y list contains corresponding
+#numerical values that represent the y-axis. The plt.scatter(x, y) function creates a scatter plot by placing
+#each pair of values (x, y) as an individual point on the graph. Each point represents the relationship between
+#the corresponding values from the two lists. Scatter plots are useful for identifying patterns, relationships,
+#trends, and variations between two numerical variables. Finally, plt.show() displays the scatter plot on the screen. 
+#Overall, this code provides a simple example of using a scatter plot to visualize the relationship between two sets of numerical data.
 
 
 
